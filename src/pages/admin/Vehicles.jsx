69afdaf4ffbc55b93f08b47891b06a49e7733bf9
Santiago397 <santilopez397@gmail.com>
2022-02-11 21:37:28 -0500

    Mockup create new vehicle

--- src/pages/admin/Vehicles.jsx
+++ src/pages/admin/Vehicles.jsx
@@ -2,61 +2,25 @@ import React, { useEffect, useState } from 'react'
 
 const Vehicles = () => {
 
-  const [vehicleType, setVehicleType] = useState("")
-  const [age, setAge] = useState(0)
-  const [isAdult, setisAdult] = useState(true)
-
-  useEffect(() => {
-    //console.log("Hola wapos");
-  }, [])
-
-  useEffect(() => {
-
-    if (age >= 18) {
-      setisAdult(true)
-    } else {
-      setisAdult(false)
-    }
-  }, [age])
-
-  const sendData = () => {
-    console.log(vehicleType);
-  }
-  /**
-   * <input onChange={(e)=>{setVehicleType(e.target.value)}} value={vehicleType} type="text" placeholder='Tipo Vehiculo ' />
-      <input type="text" placeholder='Propietario Vehiculo' />
-      <input type="text" placeholder='Placa Vehiculo' />
-      <button onClick={sendData} type="submit">Send</button>
-   * 
-  */
+  
 
 
 
   return (
-    <div className='flex flex-col '>
-      <form className='flex flex-col'>
-        <label>
-          Please, write your age
-          <input
-            onChange={(e) => { setAge(e.target.value) }}
-            max='99' min='1'
-            name='age'
-            value={age}
-            type='number'
-            className='border border-gray-500 rounded p-0.5 mx-3 appearance-none focus:outline-none'
-          />
-        </label>
-        <button onClick={()=>{console.log("")}} className='border bg-indigo-600 border-gray-400 text-white rounded my-3 py-2'>Enter</button>
-        {
-          isAdult ? <span className='text-2xl text-green-600'>
-            ¡You are an adult!
-          </span> : <span className='text-2xl text-red-600'>
-            ¡You aren't an adult!
-          </span>
-        }
+    <div className='flex flex-col w-full h-full'>
+      <div className='flex flex-col'>
+        <h2 className='text-2xl font-extrabold text-gray-800 text-center m-5'>Vehicles' Administrator</h2>
+        <button className='bg-indigo-700 text-white rounded-md p-2 mx-4 font-semibold w-40 '>New Vehicle</button>
+      </div>
+      <div className='grid grid-cols-2 p-10 '>
+        <input className='border border-gray-600 p-2 rounded-md m-2 focus:outline-none' type="text" />
+        <input className='border border-gray-600 p-2 rounded-md m-2 focus:outline-none' type="number" />
+        <input className='border border-gray-600 p-2 rounded-md m-2 focus:outline-none' type="text" />
+        <input className='border border-gray-600 p-2 rounded-md m-2 focus:outline-none' type="text" />
+        <input className='border border-gray-600 p-2 rounded-md m-2 focus:outline-none' type="text" />
 
-
-      </form>
+      </div>
+        <button className='bg-green-600 rounded-md text-white font-semibold w-40 p-2 mx-4 '>Save</button>
     </div>
   )
 }
