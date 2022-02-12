de1a017ff4b8da261626125a6936dde8ca7b05c7
Santiago397 <santilopez397@gmail.com>
2022-02-11 19:48:12 -0500

    explanation render conditional

--- src/pages/admin/Vehicles.jsx
+++ src/pages/admin/Vehicles.jsx
@@ -3,25 +3,60 @@ import React, { useEffect, useState } from 'react'
 const Vehicles = () => {
 
   const [vehicleType, setVehicleType] = useState("")
+  const [age, setAge] = useState(0)
+  const [isAdult, setisAdult] = useState(true)
 
   useEffect(() => {
-    console.log("Hola wapos");
+    //console.log("Hola wapos");
   }, [])
 
   useEffect(() => {
-    console.log("type: ", vehicleType);
-  }, [vehicleType])
+
+    if (age >= 18) {
+      setisAdult(true)
+    } else {
+      setisAdult(false)
+    }
+  }, [age])
 
   const sendData = () => {
     console.log(vehicleType);
   }
-
-  return (
-    <div className='flex flex-col '>
-      <input onChange={(e)=>{setVehicleType(e.target.value)}} value={vehicleType} type="text" placeholder='Tipo Vehiculo ' />
+  /**
+   * <input onChange={(e)=>{setVehicleType(e.target.value)}} value={vehicleType} type="text" placeholder='Tipo Vehiculo ' />
       <input type="text" placeholder='Propietario Vehiculo' />
       <input type="text" placeholder='Placa Vehiculo' />
       <button onClick={sendData} type="submit">Send</button>
+   * 
+  */
+
+
+
+  return (
+    <div className='flex flex-col '>
+      <form className='flex flex-col'>
+        <label>
+          Please, write your age
+          <input
+            onChange={(e) => { setAge(e.target.value) }}
+            max='99' min='1'
+            name='age'
+            value={age}
+            type='number'
+            className='border border-gray-500 rounded p-0.5 mx-3 appearance-none focus:outline-none'
+          />
+        </label>
+        <button onClick={()=>{console.log("")}} className='border bg-indigo-600 border-gray-400 text-white rounded my-3 py-2'>Enter</button>
+        {
+          isAdult ? <span className='text-2xl text-green-600'>
+            ¡You are an adult!
+          </span> : <span className='text-2xl text-red-600'>
+            ¡You aren't an adult!
+          </span>
+        }
+
+
+      </form>
     </div>
   )
 }
