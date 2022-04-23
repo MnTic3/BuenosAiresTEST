d01df8e3eb54c5424fee1b7f3ffb144079fa6513
Santiago397 <santilopez397@gmail.com>
2022-04-22 20:21:02 -0500

    all vehicles list responsive

--- src/pages/admin/Vehicles.jsx
+++ src/pages/admin/Vehicles.jsx
@@ -168,34 +168,53 @@ const TableVehicles = ({ listVehiclesBack, setVechicleList }) => {
         type="text"
         onChange={(e) => setSearch(e.target.value)}
         placeholder='Search'
-        className='border border-gray-700 mb-3 px-2 focus:outline-none '
+        className='border border-gray-700 mb-3 px-2 rounded-md focus:outline-none '
       />
-      <form ref={form} onSubmit={editSubmit} className='flex flex-col w-4/5'>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Model</th>
-              <th>Brand</th>
-              <th>Options</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              filterList.map((vehicle) => {
-                return (
-                  <RowVehicle key={vehicle._id} vehicle={vehicle} allVehicles={listVehiclesBack} setVechicles={setVechicleList} />
-                )
-              })
-            }
+      <div className='hidden md:flex w-full justify-center'>
+        <form ref={form} onSubmit={editSubmit} className='flex flex-col w-4/5'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Model</th>
+                <th>Brand</th>
+                <th>Options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                filterList.map((vehicle) => {
+                  return (
+                    <RowVehicle key={vehicle._id} vehicle={vehicle} allVehicles={listVehiclesBack} setVechicles={setVechicleList} />
+                  )
+                })
+              }
 
-          </tbody>
-        </table>
-      </form>
+            </tbody>
+          </table>
+        </form>
+      </div>
+      <div className='flex flex-col w-full md:hidden'>
+        {filterList.map((vehicle, index) => {
+          return (
+            <div key={vehicle._id} className="flex flex-col m-3 bg-gray-200  rounded-lg p-4 shadow-xl">
+              <div className='border-b border-gray-400 text-right mb-3'>
+                <span>{index + 1}</span>
+
+              </div>
+              <div className='flex flex-col'>
+                <span>{vehicle.vehName}</span>
+                <span>{vehicle.vehBrand}</span>
+                <span>{vehicle.vehModel}</span>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
     </div>
   )
 }
-
 
 const RowVehicle = ({ vehicle, allVehicles, setVechicles }) => {
   const [isEdit, setIsEdit] = useState()
